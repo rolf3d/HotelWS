c1371ae13f6786866c4e9651cffae0175d66524b
Rolf <rolf3d@gmail.com>
2017-03-30 10:18:04 +0200

    Lavet webservice til hotel. oprettet data og klasser ud fra Hoteldatabasen. Oprettet Controllers.

---
+++ HotelWS/Controllers/BookingsController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using HotelWS;
+
+namespace HotelWS.Controllers
+{
+    public class BookingsController : ApiController
+    {
+        private HotelDBContext db = new HotelDBContext();
+
+        // GET: api/Bookings
+        public IQueryable<Booking> GetBookings()
+        {
+            return db.Bookings;
+        }
+
+        // GET: api/Bookings/5
+        [ResponseType(typeof(Booking))]
+        public async Task<IHttpActionResult> GetBooking(int id)
+        {
+            Booking booking = await db.Bookings.FindAsync(id);
+            if (booking == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(booking);
+        }
+
+        // PUT: api/Bookings/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutBooking(int id, Booking booking)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != booking.Booking_id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(booking).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BookingExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Bookings
+        [ResponseType(typeof(Booking))]
+        public async Task<IHttpActionResult> PostBooking(Booking booking)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Bookings.Add(booking);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = booking.Booking_id }, booking);
+        }
+
+        // DELETE: api/Bookings/5
+        [ResponseType(typeof(Booking))]
+        public async Task<IHttpActionResult> DeleteBooking(int id)
+        {
+            Booking booking = await db.Bookings.FindAsync(id);
+            if (booking == null)
+            {
+                return NotFound();
+            }
+
+            db.Bookings.Remove(booking);
+            await db.SaveChangesAsync();
+
+            return Ok(booking);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool BookingExists(int id)
+        {
+            return db.Bookings.Count(e => e.Booking_id == id) > 0;
+        }
+    }
+}
---
+++ HotelWS/Controllers/HotelsController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using HotelWS;
+
+namespace HotelWS.Controllers
+{
+    public class HotelsController : ApiController
+    {
+        private HotelDBContext db = new HotelDBContext();
+
+        // GET: api/Hotels
+        public IQueryable<Hotel> GetHotels()
+        {
+            return db.Hotels;
+        }
+
+        // GET: api/Hotels/5
+        [ResponseType(typeof(Hotel))]
+        public async Task<IHttpActionResult> GetHotel(int id)
+        {
+            Hotel hotel = await db.Hotels.FindAsync(id);
+            if (hotel == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(hotel);
+        }
+
+        // PUT: api/Hotels/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutHotel(int id, Hotel hotel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != hotel.Hotel_No)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(hotel).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!HotelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Hotels
+        [ResponseType(typeof(Hotel))]
+        public async Task<IHttpActionResult> PostHotel(Hotel hotel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Hotels.Add(hotel);
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (HotelExists(hotel.Hotel_No))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = hotel.Hotel_No }, hotel);
+        }
+
+        // DELETE: api/Hotels/5
+        [ResponseType(typeof(Hotel))]
+        public async Task<IHttpActionResult> DeleteHotel(int id)
+        {
+            Hotel hotel = await db.Hotels.FindAsync(id);
+            if (hotel == null)
+            {
+                return NotFound();
+            }
+
+            db.Hotels.Remove(hotel);
+            await db.SaveChangesAsync();
+
+            return Ok(hotel);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool HotelExists(int id)
+        {
+            return db.Hotels.Count(e => e.Hotel_No == id) > 0;
+        }
+    }
+}
